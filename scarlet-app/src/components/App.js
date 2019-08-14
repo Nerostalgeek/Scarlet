dcc7671cd0626154d6bd8fcc98e5506dbcea788b
Henri Heymans <heymanshenri@gmail.com>
2019-08-14 11:51:49 +0200

    Init navbar with links and a sample logo. Non responsive at this time

---
+++ scarlet-app/src/components/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import Navbar from "./Navbar";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    navbarLinks: [
+      {
+        name: "Accueil",
+        url: "#",
+        customId: ""
+      },
+      {
+        name: "Louer une voiture",
+        url: "#",
+        customId: ""
+      },
+      {
+        name: "Mettre ma voiture en location",
+        url: "#",
+        customId: ""
+      },
+      {
+        name: "Comment ça marche",
+        url: "#",
+        customId: ""
+      },
+      {
+        name: "Connexion",
+        url: "#",
+        customId: ""
+      },
+      {
+        name: "Contact",
+        url: "#",
+        customId: ""
+      }
+    ]
+  };
+  render() {
+    return (
+      <div className="App">
+        <Navbar links={this.state.navbarLinks} />
+      </div>
+    );
+  }
+}
+
+export default App;
+
