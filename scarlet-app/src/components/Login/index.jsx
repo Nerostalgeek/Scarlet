0138dceea9669e5a9999c306d88ce89268dcd083
Estezet Nicolas <estezet.n@gmail.com>
2019-08-23 15:32:08 +0200

    added timestaamp in car

--- scarlet-app/src/components/Login/index.jsx
+++ scarlet-app/src/components/Login/index.jsx
@@ -4,43 +4,47 @@ import GoogleLogin from "react-google-login";
 import "./login.css";
 
 class Login extends Component {
-  state = {};
+    state = {};
 
-  render() {
-    return (
-      <div className="l-signin">
-        <div className="signin-buttons">
-          <FacebookLogin
-            cssClass="signin-item-button facebook-button"
-            appId="460733124511623"
-            autoLoad={true}
-            fields="name,email,picture"
-            callback={this.responseFacebook}
-            icon="fa-facebook"
-            textButton="Connexion avec Facebook"
-          />
+    responseFacebook = () => {
+        console.log('Bouton Facebook cliqué');
+    }
 
-          <GoogleLogin
-            render={renderProps => (
-              <button
-                className="signin-item-button google-button"
-                onClick={renderProps.onClick}
-                disabled={renderProps.disabled}
-                icon="fa-google"
-              >
-                Connexion avec Google
-              </button>
-            )}
-            clientId="399431441363-edqd6pdbm8icg7gjm7d27sffg2uuntha.apps.googleusercontent.com" //CLIENTID NOT CREATED YET
-            buttonText="Login with Google"
-            onSuccess={this.responseGoogle}
-            onFailure={this.responseGoogle}
-          />
-          <button className="signin-item-button">S'inscrire par Mail</button>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="l-signin">
+                <div className="signin-buttons">
+                    <FacebookLogin
+                        cssClass="signin-item-button facebook-button"
+                        appId="460733124511623"
+                        autoLoad={true}
+                        fields="name,email,picture"
+                        callback={this.responseFacebook}
+                        icon="fa-facebook"
+                        textButton="Connexion avec Facebook"
+                    />
+
+                    <GoogleLogin
+                        render={renderProps => (
+                            <button
+                                className="signin-item-button google-button"
+                                onClick={renderProps.onClick}
+                                disabled={renderProps.disabled}
+                                icon="fa-google"
+                            >
+                                Connexion avec Google
+                            </button>
+                        )}
+                        clientId="399431441363-edqd6pdbm8icg7gjm7d27sffg2uuntha.apps.googleusercontent.com" //CLIENTID NOT CREATED YET
+                        buttonText="Login with Google"
+                        onSuccess={this.responseGoogle}
+                        onFailure={this.responseGoogle}
+                    />
+                    <button className="signin-item-button">Se connecter par Email</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Login;
