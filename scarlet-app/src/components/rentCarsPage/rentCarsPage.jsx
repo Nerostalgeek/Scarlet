d98dc2cb9e8f231cc367d1152347ca31b82b2046
henheym <heymanshenri@gmail.com>
2019-09-16 21:22:08 +0200

    Rent Cars PAge : Init header + cars type

--- scarlet-app/src/components/rentCarsPage/rentCarsPage.jsx
+++ scarlet-app/src/components/rentCarsPage/rentCarsPage.jsx
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import fourxfour from "../../img/assets/rent-page/cars-icons/4x4.png";
+import citadine from "../../img/assets/rent-page/cars-icons/citadine.png";
+import spacieuse from "../../img/assets/rent-page/cars-icons/spacieuse.png";
+import utilitaire from "../../img/assets/rent-page/cars-icons/utilitaire.png";
+
 import "./rentCarsPage.css";
 class rentCarsPage extends Component {
   state = {};
@@ -7,7 +12,24 @@ class rentCarsPage extends Component {
       <div className="l-rentCars">
         <div className="rentCars-header"></div>
         <div className="rentCars-allCars">
-          <div className="allCars-type"></div>
+          <div className="allCars-type">
+            <div className="cars-type-item">
+              <img className="cars-type-item-image" src={citadine} alt="" />
+              <h3>CITADINE</h3>
+            </div>
+            <div className="cars-type-item">
+              <img className="cars-type-item-image" src={spacieuse} alt="" />
+              <h3>SPACIEUSE</h3>
+            </div>
+            <div className="cars-type-item">
+              <img className="cars-type-item-image" src={fourxfour} alt="" />
+              <h3>4 X 4</h3>
+            </div>
+            <div className="cars-type-item">
+              <img className="cars-type-item-image" src={utilitaire} alt="" />
+              <h3>UTILITAIRE</h3>
+            </div>
+          </div>
           <div className="allCars-container">
             <div className="allCars-filters"></div>
             <div className="allCars-cars">
