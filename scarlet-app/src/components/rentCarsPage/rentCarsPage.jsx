4884ca90e2c9354a61d23bbb030c86a1d68d1317
henriheymans <heymanshenri@gmail.com>
2019-09-16 20:09:31 +0200

    Start page cars front end

---
+++ scarlet-app/src/components/rentCarsPage/rentCarsPage.jsx
@@ -0,0 +1,175 @@
+import React, { Component } from "react";
+import "./rentCarsPage.css";
+class rentCarsPage extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="l-rentCars">
+        <div className="rentCars-header"></div>
+        <div className="rentCars-allCars">
+          <div className="allCars-type"></div>
+          <div className="allCars-container">
+            <div className="allCars-filters"></div>
+            <div className="allCars-cars">
+              <div class="rentCars-car-item">
+                <div className="car-item-image">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2017/02/27/08/27/car-2102367_960_720.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="car-item-description">
+                  <div className="car-item-description-left">
+                    <h3>Fiat 500</h3>
+                    <h3>5 / 5</h3>
+                  </div>
+                  <div className="car-item-description-right">
+                    <p>65€ / Jour</p>
+                  </div>
+                </div>
+              </div>
+              <div class="rentCars-car-item">
+                <div className="car-item-image">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2017/02/27/08/27/car-2102367_960_720.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="car-item-description">
+                  <div className="car-item-description-left">
+                    <h3>Fiat 500</h3>
+                    <h3>5 / 5</h3>
+                  </div>
+                  <div className="car-item-description-right">
+                    <p>65€ / Jour</p>
+                  </div>
+                </div>
+              </div>{" "}
+              <div class="rentCars-car-item">
+                <div className="car-item-image">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2017/02/27/08/27/car-2102367_960_720.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="car-item-description">
+                  <div className="car-item-description-left">
+                    <h3>Fiat 500</h3>
+                    <h3>5 / 5</h3>
+                  </div>
+                  <div className="car-item-description-right">
+                    <p>65€ / Jour</p>
+                  </div>
+                </div>
+              </div>{" "}
+              <div class="rentCars-car-item">
+                <div className="car-item-image">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2017/02/27/08/27/car-2102367_960_720.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="car-item-description">
+                  <div className="car-item-description-left">
+                    <h3>Fiat 500</h3>
+                    <h3>5 / 5</h3>
+                  </div>
+                  <div className="car-item-description-right">
+                    <p>65€ / Jour</p>
+                  </div>
+                </div>
+              </div>{" "}
+              <div class="rentCars-car-item">
+                <div className="car-item-image">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2017/02/27/08/27/car-2102367_960_720.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="car-item-description">
+                  <div className="car-item-description-left">
+                    <h3>Fiat 500</h3>
+                    <h3>5 / 5</h3>
+                  </div>
+                  <div className="car-item-description-right">
+                    <p>65€ / Jour</p>
+                  </div>
+                </div>
+              </div>{" "}
+              <div class="rentCars-car-item">
+                <div className="car-item-image">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2017/02/27/08/27/car-2102367_960_720.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="car-item-description">
+                  <div className="car-item-description-left">
+                    <h3>Fiat 500</h3>
+                    <h3>5 / 5</h3>
+                  </div>
+                  <div className="car-item-description-right">
+                    <p>65€ / Jour</p>
+                  </div>
+                </div>
+              </div>{" "}
+              <div class="rentCars-car-item">
+                <div className="car-item-image">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2017/02/27/08/27/car-2102367_960_720.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="car-item-description">
+                  <div className="car-item-description-left">
+                    <h3>Fiat 500</h3>
+                    <h3>5 / 5</h3>
+                  </div>
+                  <div className="car-item-description-right">
+                    <p>65€ / Jour</p>
+                  </div>
+                </div>
+              </div>{" "}
+              <div class="rentCars-car-item">
+                <div className="car-item-image">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2017/02/27/08/27/car-2102367_960_720.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="car-item-description">
+                  <div className="car-item-description-left">
+                    <h3>Fiat 500</h3>
+                    <h3>5 / 5</h3>
+                  </div>
+                  <div className="car-item-description-right">
+                    <p>65€ / Jour</p>
+                  </div>
+                </div>
+              </div>{" "}
+              <div class="rentCars-car-item">
+                <div className="car-item-image">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2017/02/27/08/27/car-2102367_960_720.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="car-item-description">
+                  <div className="car-item-description-left">
+                    <h3>Fiat 500</h3>
+                    <h3>5 / 5</h3>
+                  </div>
+                  <div className="car-item-description-right">
+                    <p>65€ / Jour</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default rentCarsPage;
