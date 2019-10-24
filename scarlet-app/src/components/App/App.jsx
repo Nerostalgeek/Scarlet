6c3b4fb5d43a71e50fc094891f415693d3e55991
henriheymans <heymanshenri@gmail.com>
2019-10-24 12:49:59 +0200

    resolve conflict with develop

--- scarlet-app/src/components/App/App.jsx
+++ scarlet-app/src/components/App/App.jsx
@@ -7,6 +7,7 @@ import NavBar from "../NavBar/NavBar";
 import HomePage from "../HomePage/HomePage";
 import { history } from "../../helpers";
 import RentCarsPage from "../RentCarsPage/RentCarsPage";
+import RentCar from "../RentCar/RentCar";
 import Dashboard from "../Dashboard/Dashboard";
 import Footer from "../Footer/Footer";
 import Login from "../Login/Login";
@@ -34,6 +35,7 @@ const App = () => {
         <Route path="/login" component={Login} />
         <Route path="/reset-password" component={ResetPassword} />
         <Route path="/rent-cars" component={RentCarsPage} />
+        <Route path="/rent-my-car" component={RentCar} />
         <Route path="/validate-account" component={validateAccount} />
         <PrivateRoute path="/dashboard" component={Dashboard} />
         <PrivateRoute path="/logout" />
--- scarlet-app/src/components/RentCar/RentCar.jsx
+++ scarlet-app/src/components/RentCar/RentCar.jsx
@@ -4,7 +4,7 @@ import "./RentCar.css";
 
 import * as vehicles_props from "../../../databases/vehicles_props.json";
 
-import LoginButtons from "../Login/LoginButtons/LoginButtons";
+// import LoginButtons from "../Login/LoginButtons/LoginButtons";
 
 const RentCar = () => {
   const userIsConnected = useSelector(state => state.authentication.loggedIn);
@@ -24,27 +24,35 @@ const RentCar = () => {
 
   populateBrandSelect();
 
-  const inputSelected = (event) => {
+  const inputSelected = event => {
+    let nextElementNumber = event.target.getAttribute("data-number");
 
-    let nextElementNumber = event.target.getAttribute('data-number');
-
-    const previousFieldName = event.target.getAttribute('data-field-name');
+    const previousFieldName = event.target.getAttribute("data-field-name");
 
     nextElementNumber++;
 
-    const nextElementToShow = document.getElementById(`rentForm${nextElementNumber}`);
-    
+    const nextElementToShow = document.getElementById(
+      `rentForm${nextElementNumber}`
+    );
+
     const selectToPopulate = nextElementToShow.childNodes[1];
 
-    const fieldToShow = selectToPopulate.getAttribute('data-field-name');
+    const fieldToShow = selectToPopulate.getAttribute("data-field-name");
 
     const previousFieldValue = event.target.value;
 
     let nextArr = [];
 
-    for (let arrayIterator = 0; arrayIterator < dbVehicles.default.length; arrayIterator++) {
-      if(dbVehicles.default[arrayIterator][previousFieldName] === previousFieldValue) {
-        if(!nextArr.includes(dbVehicles.default[arrayIterator][fieldToShow])) {
+    for (
+      let arrayIterator = 0;
+      arrayIterator < dbVehicles.default.length;
+      arrayIterator++
+    ) {
+      if (
+        dbVehicles.default[arrayIterator][previousFieldName] ===
+        previousFieldValue
+      ) {
+        if (!nextArr.includes(dbVehicles.default[arrayIterator][fieldToShow])) {
           nextArr.push(dbVehicles.default[arrayIterator][fieldToShow]);
         }
       }
@@ -53,107 +61,165 @@ const RentCar = () => {
     let selectLength = selectToPopulate.length;
 
     // EMPTY OPTIONS IF SELECT CHANGE (ça marche po)
-    for(let selectIterator = 0; selectIterator < selectLength; selectIterator++) {
+    for (
+      let selectIterator = 0;
+      selectIterator < selectLength;
+      selectIterator++
+    ) {
       selectToPopulate.options[selectIterator] = null;
     }
 
-    for(let pushOptionIterator = 0; pushOptionIterator < nextArr.length; pushOptionIterator++) {
-      let newOption = document.createElement('option');
+    for (
+      let pushOptionIterator = 0;
+      pushOptionIterator < nextArr.length;
+      pushOptionIterator++
+    ) {
+      let newOption = document.createElement("option");
 
-      newOption.appendChild( document.createTextNode(`${nextArr[pushOptionIterator]}`));
+      newOption.appendChild(
+        document.createTextNode(`${nextArr[pushOptionIterator]}`)
+      );
       newOption.value = `${nextArr[pushOptionIterator]}`;
 
-      selectToPopulate.appendChild(newOption)
+      selectToPopulate.appendChild(newOption);
     }
 
     nextElementToShow.style.display = "block";
-  }
+  };
 
   const listItems = brandArr.map(brand => <option key={brand}>{brand}</option>);
 
-  if (userIsConnected) {
+
     return (
       <div className="l-rent-car">
         <div className="rent-car-content">
           <div className="rent-car-form">
             <form action="">
-              <div  id="rentForm1" className="form-rent-car-item">
+              <div id="rentForm1" className="form-rent-car-item">
                 <h4>Marque du véhicule :</h4>
-                <select data-field-name="make" data-number="1" onChange={inputSelected} className="rent-car-input rent-select">
+                <select
+                  data-field-name="make"
+                  data-number="1"
+                  onChange={inputSelected}
+                  className="rent-car-input rent-select"
+                >
                   <option>Sélectionnez la marque de votre véhicule</option>
                   {listItems}
                 </select>
               </div>
 
-            <div class="form-rent-row">
-              <div id="rentForm2" className="form-rent-car-item">
-                <h4>Modèle du véhicule :</h4>
-                <select data-field-name="model" data-number="2" onChange={inputSelected} className="rent-car-input rent-select">
-                  <option>Sélectionnez le modèle de votre véhicule</option>
-                </select>
-              </div>
-              <div id="rentForm3" className="form-rent-car-item">
-                <h4>Année du véhicule :</h4>
-                <select data-field-name="year" data-number="3" className="rent-car-input rent-select">
-                  <option>Sélectionnez le modèle de votre véhicule</option>
-                </select>
-              </div>
+              <div class="form-rent-row">
+                <div id="rentForm2" className="form-rent-car-item">
+                  <h4>Modèle du véhicule :</h4>
+                  <select
+                    data-field-name="model"
+                    data-number="2"
+                    onChange={inputSelected}
+                    className="rent-car-input rent-select"
+                  >
+                    <option>Sélectionnez le modèle de votre véhicule</option>
+                  </select>
+                </div>
+                <div id="rentForm3" className="form-rent-car-item">
+                  <h4>Année du véhicule :</h4>
+                  <select
+                    data-field-name="year"
+                    data-number="3"
+                    className="rent-car-input rent-select"
+                  >
+                    <option>Sélectionnez le modèle de votre véhicule</option>
+                  </select>
+                </div>
               </div>
               <div class="form-rent-row">
                 <div id="rentForm4" className="form-rent-car-item">
                   <h4>Moteur :</h4>
-                  <select data-field-name="year" data-number="4"  className="rent-car-input rent-select">
-                    <option>Sélectionnez le type de moteur de votre véhicule</option>
+                  <select
+                    data-field-name="year"
+                    data-number="4"
+                    className="rent-car-input rent-select"
+                  >
+                    <option>
+                      Sélectionnez le type de moteur de votre véhicule
+                    </option>
                     <option>Essence</option>
                     <option>Disel</option>
                     <option>Electrique</option>
                   </select>
                 </div>
-              <div id="rentForm5" className="form-rent-car-item">
-                <h4>Transmission</h4>
-                <select data-field-name="engine" data-number="5"  className="rent-car-input rent-select">
-                  <option>Sélectionnez le type de moteur de votre véhicule</option>
-                  <option>Manuelle</option>
-                  <option>Automatique</option>
-                </select>
-              </div>
+                <div id="rentForm5" className="form-rent-car-item">
+                  <h4>Transmission</h4>
+                  <select
+                    data-field-name="engine"
+                    data-number="5"
+                    className="rent-car-input rent-select"
+                  >
+                    <option>
+                      Sélectionnez le type de moteur de votre véhicule
+                    </option>
+                    <option>Manuelle</option>
+                    <option>Automatique</option>
+                  </select>
+                </div>
               </div>
               <div class="form-rent-row">
                 <div id="rentForm6" className="form-rent-car-item">
                   <h4>Nombre de kilomètres</h4>
-                  <select data-field-name="kilometers" data-number="6"  className="rent-car-input rent-select">
-                    <option>Sélectionnez le nombre de kilomètres de votre véhicule</option>
+                  <select
+                    data-field-name="kilometers"
+                    data-number="6"
+                    className="rent-car-input rent-select"
+                  >
+                    <option>
+                      Sélectionnez le nombre de kilomètres de votre véhicule
+                    </option>
                     <option>0 - 50 000</option>
                     <option>50 000 - 100 000</option>
                     <option>100 000 - 200 000</option>
                     <option>+ 200 000</option>
-                  </select>                
+                  </select>
                 </div>
                 <div id="rentForm7" className="form-rent-car-item">
                   <h4>Nombre de sièges</h4>
-                  <input type="text" data-field-name="seats" data-number="7" className="rent-car-input" />
+                  <input
+                    type="text"
+                    data-field-name="seats"
+                    data-number="7"
+                    className="rent-car-input"
+                  />
                 </div>
               </div>
               <div class="form-rent-row">
                 <div id="rentForm8" className="form-rent-car-item">
                   <h4>Année d'enregistrement</h4>
-                  <input type="text" data-field-name="kilometers" data-number="8" className="rent-car-input" />
+                  <input
+                    type="text"
+                    data-field-name="kilometers"
+                    data-number="8"
+                    className="rent-car-input"
+                  />
                 </div>
                 <div id="rentForm9" className="form-rent-car-item">
                   <h4>Pays d'enregistrement</h4>
-                  <input type="text" data-field-name="seats" data-number="9" className="rent-car-input" />
+                  <input
+                    type="text"
+                    data-field-name="seats"
+                    data-number="9"
+                    className="rent-car-input"
+                  />
                 </div>
               </div>
-              <input className="rentSubmitInput" type="submit" value="Valider" />
+              <input
+                className="rentSubmitInput"
+                type="submit"
+                value="Valider"
+              />
             </form>
           </div>
           <div className="rent-car-image"></div>
         </div>
       </div>
     );
-  } else {
-    return <LoginButtons />;
-  }
 };
 
 export default RentCar;
